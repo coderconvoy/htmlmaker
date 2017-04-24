f29b23be343a7e2f3067095108bdcc4f3a25b940
coderconvoy <thebeaky@psmail.net>
2017-04-24 13:26:09 +0100

    Added NewParent Method for quick construction

--- maker.go
+++ maker.go
@@ -1,5 +1,7 @@
 package htmlmaker
 
+import "strings"
+
 type Tag struct {
 	TType    string
 	Attrs    map[string]string
@@ -9,6 +11,12 @@ type Tag struct {
 
 func NewTag(kind string, s ...string) *Tag {
 	res := &Tag{kind, make(map[string]string), make([]*Tag, 0), ""}
+	res.AddAttrs(s...)
+	return res
+}
+
+func NewParent(kind string, children []*Tag, s ...string) *Tag {
+	res := &Tag{kind, make(map[string]string), children, ""}
 	res.AddAttrs(s...)
 	return res
 }
@@ -34,7 +42,7 @@ func NewText(s string) *Tag {
 //return the top page object, and the body
 func NewPage(ss ...string) (*Tag, *Tag) {
 	//fill params
-	title, css, js = "", "", ""
+	title, css, js := "", "", ""
 	if len(ss) > 0 {
 		title = ss[0]
 	}
@@ -46,24 +54,35 @@ func NewPage(ss ...string) (*Tag, *Tag) {
 	}
 
 	//create bases
-	dt := NewTag("DOCTYPE", "html")
+	dt := NewTag("!DOCTYPE", "html")
 	mh := NewTag("html")
-	mh.AddChildren(
+	head := NewTag("head")
+	body := NewTag("body")
+	mh.AddChildren(head, body)
+	head.AddChildren(
 		NewTextTag("title", title),
 		NewTag("meta", "charset", "utf-8"),
 	)
+	for _, s := range strings.Split(css, ",") {
+		head.AddChildren(NewTag("link", "rel", "stylesheet", "type", "text/css", "href", s))
+	}
+
+	for _, _ = range js {
+	}
+
+	return NewParent("page", []*Tag{dt, mh}), body
 
 }
 
 //AddAttrs is a function for the super lazy.
-//Simply strings are paired as k-v, odds at the end become innerHTML
+//Simply strings are paired as k-v, Odds at the end, become unstrung
 func (self *Tag) AddAttrs(s ...string) {
 	ls := len(s)
 	for i := 0; i+1 < ls; i += 2 {
 		self.Attrs[s[i]] = s[i+1]
 	}
 	if ls%2 == 1 {
-		self.Inner = s[ls-1]
+		self.Attrs[s[len(s)-1]] = ""
 	}
 
 }
@@ -73,25 +92,32 @@ func (self *Tag) AddChildren(ts ...*Tag) {
 }
 
 func Childless(ttype string) bool {
-	if ttype == "br" {
-		return true
-	}
-	if ttype == "img" {
-		return true
+	ttype = strings.ToLower(ttype)
+	childless := []string{"br", "img", "meta", "!doctype"}
+	for _, s := range childless {
+		if s == ttype {
+			return true
+		}
 	}
 	return false
-
 }
 func (self *Tag) String() string {
 	return self.toString("")
 }
 
 func (self *Tag) toString(pre string) string {
-	res := pre + "<" + self.TType
-	for k, v := range self.Attrs {
-		res += " " + k + "=" + "\"" + v + "\""
+	res := ""
+	if self.TType != "page" {
+		res = pre + "<" + self.TType
+		for k, v := range self.Attrs {
+			if v == "" {
+				res += " " + k
+				continue
+			}
+			res += " " + k + "=" + "\"" + v + "\""
+		}
+		res += ">"
 	}
-	res += ">"
 	if Childless(self.TType) {
 		return res + "\n"
 	}
@@ -107,7 +133,9 @@ func (self *Tag) toString(pre string) string {
 		}
 		res += pre
 	}
-	res += "</" + self.TType + ">\n"
+	if self.TType != "page" {
+		res += "</" + self.TType + ">\n"
+	}
 
 	return res
 
--- maker_test.go
+++ maker_test.go
@@ -5,9 +5,18 @@ import (
 	"testing"
 )
 
-func Test_new(t *testing.T) {
+func test_new(t *testing.T) {
 	a := NewTag("button", "id", "btn_hello")
 	a.AddChildren(NewTag("img", "id", "img_4", "class", "maker"))
 	a.AddChildren(NewTag("button", "POOOO"))
 	fmt.Println(a)
+}
+
+func Test_Page(t *testing.T) {
+	fmt.Println("PAGE")
+	p, b := NewPage("gofish", "s/poo/g.css,poopopp.css")
+	fmt.Println(p)
+	fmt.Println("BODY")
+	fmt.Println(b)
+
 }
